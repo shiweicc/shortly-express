6d8d2aef2ae26b4e2ad055c0f6e51e7cae52473f
Shiwei Chen <shiweichenus@gmail.com>
2022-05-10 22:30:43 -0500

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,28 @@
 const parseCookies = (req, res, next) => {
+  var cookie = req.headers;
+  if (cookie.cookie === undefined) {
+    next();
+  } else {
+    var cookieObj = {};
+    var cookieValue = cookie.cookie;
+    if (cookieValue.indexOf('; ') !== -1) {
+      var array = cookieValue.split('; ');
+      array.forEach(cookie => {
+        var index = cookie.indexOf('=');
+        var key = cookie.slice(0, index);
+        var value = cookie.slice(index + 1);
+        cookieObj[key] = value;
+      });
+      req.cookies = cookieObj;
+      next();
+    } else {
+      var index = cookieValue.indexOf('=');
+      var key = cookieValue.slice(0, index);
+      var value = cookieValue.slice(index + 1);
+      cookieObj[key] = value;
+      req.cookies = cookieObj;
+      next();
+    }
+  }
 };
-
 module.exports = parseCookies;
